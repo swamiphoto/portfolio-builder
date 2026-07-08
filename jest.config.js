c6980e0c16dc3a9eb2fa1e831c2e53cd223fc9f4
Swami Venkataramani <swami108@gmail.com>
2026-07-08 15:13:42 -0700

    test: configure Jest to resolve cheerio CommonJS entry point

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,12 @@ module.exports = createJestConfig({
   testMatch: ['<rootDir>/__tests__/**/*.test.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
+    '^cheerio$': '<rootDir>/node_modules/cheerio/dist/commonjs/index.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(cheerio|parse5|entities|dom-serializer|domelementtype|domhandler|domutils|undici)/)',
+  ],
+  testEnvironmentOptions: {
+    customExportConditions: [''],
   },
 })
